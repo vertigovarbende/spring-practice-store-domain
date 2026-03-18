3f1fbd13f1a26b6f6c478493bd354cd584e4afb8
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-03-18 21:46:28 +0300

    refactor 'BaseEntity', 'AuditingEntityListener.class'

--- src/main/java/com/deveyk/bookstore/common/repository/entity/BaseEntity.java
+++ src/main/java/com/deveyk/bookstore/common/repository/entity/BaseEntity.java
@@ -1,37 +1,39 @@
 package com.deveyk.bookstore.common.repository.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-public class BaseEntity {
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
 
-    @Column(name = "created_at")
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
     protected LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(name = "updated_at")
     protected LocalDateTime updatedAt;
 
-    @PrePersist
-    public void prePersist() {
-        if (createdAt == null) {
-            createdAt = LocalDateTime.now();
-        }
-    }
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    protected String createdBy;
 
-    @PreUpdate
-    public void preUpdate() {
-        if (updatedAt == null) {
-            updatedAt = LocalDateTime.now();
-        }
-    }
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    protected String updatedBy;
 
 }
+
+
