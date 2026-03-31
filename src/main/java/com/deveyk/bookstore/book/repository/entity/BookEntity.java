ab42c6b77a4a3a911af3ebad474db44204690dfe
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-03-31 23:13:00 +0300

    refactor: 'BookEntity' class

--- src/main/java/com/deveyk/bookstore/book/repository/entity/BookEntity.java
+++ src/main/java/com/deveyk/bookstore/book/repository/entity/BookEntity.java
@@ -1,8 +1,14 @@
 package com.deveyk.bookstore.book.repository.entity;
 
+import com.deveyk.bookstore.author.repository.entity.AuthorEntity;
+import com.deveyk.bookstore.book.model.enums.BookGenre;
+import com.deveyk.bookstore.book.model.enums.BookStatus;
 import com.deveyk.bookstore.common.repository.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "BS_BOOKS")
@@ -18,9 +24,66 @@ public class BookEntity extends BaseEntity {
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "TITLE")
+    @Column(name = "TITLE", nullable = false, length = 500)
     private String title;
 
+    @Column(name = "ISBN_10", length = 10, unique = true)
+    private String isbn10;
 
+    @Column(name = "ISBN_13", length = 13, unique = true)
+    private String isbn13;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "BS_BOOK_AUTHORS",
+            joinColumns = @JoinColumn(name = "BOOK_ID"),
+            inverseJoinColumns = @JoinColumn(name = "AUTHOR_ID")
+    )
+    private Set<AuthorEntity> authors;
+
+    @Column(name = "PUBLISHER", length = 200)
+    private String publisher;
+
+    @Column(name = "PUBLICATION_DATE")
+    private LocalDateTime publicationDate;
+
+    @Column(name = "EDITION")
+    private Integer edition;
+
+    @Column(name = "LANGUAGE", length = 50)
+    private String language;
+
+    @Column(name = "PAGE_COUNT")
+    private Integer pageCount;
+
+    @ElementCollection(targetClass = BookGenre.class, fetch = FetchType.LAZY)
+    @CollectionTable(
+            name = "BOOK_GENRES",
+            joinColumns = @JoinColumn(name = "BOOK_ID")
+    )
+    @Enumerated(EnumType.STRING)
+    @Column(name = "GENRE", nullable = false)
+    private Set<BookGenre> genres;
+
+    @Column(name = "CATEGORY", length = 100)
+    private String category;        // i am gonna create CategoryEntity
+
+    @Column(name = "STATUS")
+    @Enumerated(EnumType.STRING)
+    private BookStatus status;
+
+    // Methods
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BookEntity bookEntity)) return false;
+        return id != null && id.equals(bookEntity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 
 }
