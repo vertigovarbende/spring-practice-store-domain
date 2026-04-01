71c7de4f9afdf2bcb3206024aeb56d82ff55491e
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-01 23:56:16 +0300

    feat: add 'BookGenre' and 'BookStatus'

---
+++ src/main/java/com/deveyk/bookstore/book/model/enums/BookGenre.java
@@ -0,0 +1,14 @@
+package com.deveyk.bookstore.book.model.enums;
+
+public enum BookGenre {
+
+    FANTASY,
+    SCIENCE_FICTION,
+    ROMANCE,
+    THRILLER,
+    HORROR,
+    MYSTERY,
+    DRAMA,
+    HISTORY
+
+}
---
+++ src/main/java/com/deveyk/bookstore/book/model/enums/BookStatus.java
@@ -0,0 +1,9 @@
+package com.deveyk.bookstore.book.model.enums;
+
+public enum BookStatus {
+    AVAILABLE,
+    SOLD_OUT,
+    OUT_OF_STOCK,
+    SOLD_OUT_AND_CANCELLED,
+    CANCELLED,
+}
