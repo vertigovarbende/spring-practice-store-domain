364b70baabf59ff72e026ad8475e22b0eaf9605a
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-01 23:58:46 +0300

    refactor: refactor 'BookRepository' - @EntityGraph annotation

--- src/main/java/com/deveyk/bookstore/book/repository/BookRepository.java
+++ src/main/java/com/deveyk/bookstore/book/repository/BookRepository.java
@@ -1,12 +1,17 @@
 package com.deveyk.bookstore.book.repository;
 
 import com.deveyk.bookstore.book.repository.entity.BookEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, String> {
 
+    @EntityGraph(attributePaths = {"authors"})
+    List<BookEntity> findAll();
 
 
 }
