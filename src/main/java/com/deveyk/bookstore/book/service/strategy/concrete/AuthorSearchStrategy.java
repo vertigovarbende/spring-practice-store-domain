ccfbfb755bf81447ca026472415110916bc3af5e
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-02 15:42:24 +0300

    refactor: refactor 'AuthorSearchStrategy'

--- src/main/java/com/deveyk/bookstore/book/service/strategy/concrete/AuthorSearchStrategy.java
+++ src/main/java/com/deveyk/bookstore/book/service/strategy/concrete/AuthorSearchStrategy.java
@@ -5,21 +5,32 @@ import com.deveyk.bookstore.book.service.strategy.SearchStrategy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class AuthorSearchStrategy implements SearchStrategy {
 
+    // idk ?????
     @Override
     public List<Book> search(String searchTerm, List<Book> books) {
+        if (searchTerm == null || searchTerm.isBlank()) {
+            return List.of();
+        }
+        String normalizedSearchTerm = searchTerm.toLowerCase();
 
-        return null;
-        /*
         return books.stream()
-                .filter(book -> book.getAuthor().toLowerCase().contains(searchTerm.toLowerCase()))
+                .filter(book -> book.getAuthors().stream()
+                        .anyMatch(author -> {
+                            var name = author.getName();
+                            if (name == null) return false;
+                            return Stream.of(name.getFirstName(), name.getMiddleName(), name.getLastName(), name.getPenName())
+                                    .filter(Objects::nonNull)
+                                    .map(String::toLowerCase)
+                                    .anyMatch(s -> s.contains(normalizedSearchTerm));
+                        }))
                 .collect(Collectors.toList());
-
-         */
     }
 
     @Override
