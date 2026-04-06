a52f4c9f33e1c856a017fd457b736f4b5431a7ec
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-06 21:34:19 +0300

    refactor: refactor 'BookSearchRequest'

--- src/main/java/com/deveyk/bookstore/book/controller/request/BookSearchRequest.java
+++ src/main/java/com/deveyk/bookstore/book/controller/request/BookSearchRequest.java
@@ -1,9 +1,45 @@
 package com.deveyk.bookstore.book.controller.request;
 
-public record BookSearchRequest(
-    String searchTerm,
-    String searchType
-) {
+import com.deveyk.bookstore.common.controller.request.BsPaginationRequest;
+import com.deveyk.bookstore.common.model.Filtering;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+public class BookSearchRequest extends BsPaginationRequest {
+
+    @Valid
+    private BookSearchFilter filter;
+
+    private static final Set<String> ACCEPTED_SORT_PROPERTIES = Set.of(
+            "title",
+            "author",
+            "publicationDate"
+    );
+
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BookSearchFilter implements Filtering {
+        @NotBlank(message = "Search term cannot be blank")
+        private String searchTerm;
+        @NotBlank(message = "Search type cannot be blank")
+        private String searchType;
+    }
+
+
+    @Override
+    public boolean isOrderPropertyAccepted() {
+        return this.isPropertyAccepted(ACCEPTED_SORT_PROPERTIES);
+    }
 
 
 }
