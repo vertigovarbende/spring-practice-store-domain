449bf56ca875bc7d4e95273a90ee85e063d11755
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-07 13:45:23 +0300

    refactor: refactor 'BookSearchResponse'

--- src/main/java/com/deveyk/bookstore/book/controller/response/BookSearchResponse.java
+++ src/main/java/com/deveyk/bookstore/book/controller/response/BookSearchResponse.java
@@ -1,9 +1,51 @@
 package com.deveyk.bookstore.book.controller.response;
 
-public record BookResponse(
+import com.deveyk.bookstore.author.service.domain.AuthorName;
+import com.deveyk.bookstore.book.model.enums.BookGenre;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public record BookSearchResponse(
 
         String id,
-        String title
+        String title,
+        Set<BookSearchAuthorResponse> authors,
+        String publisher,
+        LocalDate publicationDate,
+        String edition,
+        String language,
+        int pageCount,
+        Set<BookGenre> genres,
+        String category,
+        String status
 
 ) {
+
+    @Builder
+    public record BookSearchAuthorResponse(
+            String id,
+            @JsonInclude(JsonInclude.Include.NON_NULL) // ??
+            AuthorName name,
+            boolean verified
+    ) {
+
+    }
+
 }
+    /*
+    @Getter
+    @Builder
+    public static class BookSearchResponseAuthor {
+        private String id;
+        private String firstName;
+        private String middleName;
+        private String lastName;
+        private String penName;
+        private boolean isVerified;
+    }
+    */
+
