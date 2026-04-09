0225fd93f1dfe4ab4bcd22de1c43a280b7af3e4d
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-09 21:43:00 +0300

    feat: add 'AbstractBookSearchStrategy'

---
+++ src/main/java/com/deveyk/bookstore/book/service/strategy/AbstractBookSearchStrategy.java
@@ -0,0 +1,42 @@
+package com.deveyk.bookstore.book.service.strategy;
+
+import com.deveyk.bookstore.book.model.mapper.BookEntityToDomainMapper;
+import com.deveyk.bookstore.book.repository.BookRepository;
+import com.deveyk.bookstore.book.repository.entity.BookEntity;
+import com.deveyk.bookstore.book.service.command.BookSearchCommand;
+import com.deveyk.bookstore.book.service.domain.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
+
+@RequiredArgsConstructor
+public abstract class AbstractBookSearchStrategy implements SearchStrategy {
+
+    protected final BookRepository repository;
+    protected final BookEntityToDomainMapper bookEntityToDomainMapper;
+
+    @Override
+    public Page<Book> search(BookSearchCommand command) {
+        if (command == null || command.getFilter() == null) {
+            return Page.empty();
+        }
+
+        String term = command.getFilter().getSearchTerm();
+        if (term == null || term.isBlank()) {
+            return Page.empty(command.toPageable());
+        }
+
+        // common filters (from command)
+        Specification<BookEntity> commonSpec = command.toSpecification(BookEntity.class);
+        // specific search (from strategy)
+        Specification<BookEntity> specificSpec = buildSpecification(term.toLowerCase());
+        // final specification
+        Specification<BookEntity> finalSpec = commonSpec.and(specificSpec);
+
+        return repository.findAll(finalSpec, command.toPageable())
+                .map(bookEntityToDomainMapper::map);
+    }
+
+    protected abstract Specification<BookEntity> buildSpecification(String term);
+
+}
