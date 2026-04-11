eb75a30864a08ea8a8fdc943eab0e4a480e34357
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-11 13:42:14 +0300

    feat: add 'BookCreateCommand'

---
+++ src/main/java/com/deveyk/bookstore/book/service/command/BookCreateCommand.java
@@ -0,0 +1,23 @@
+package com.deveyk.bookstore.book.service.command;
+
+import com.deveyk.bookstore.book.model.enums.BookStatus;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+public record BookCreateCommand(
+        String title,
+        BookStatus status,
+        String publisher,
+        LocalDate publicationDate,
+        String edition,
+        String language,
+        Integer pageCount,
+        String category,
+        Set<UUID> authorIds,
+        Set<String> genres
+) {
+
+
+}
