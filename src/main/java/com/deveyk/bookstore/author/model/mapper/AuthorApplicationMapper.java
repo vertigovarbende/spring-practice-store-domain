8452defd710e40b2c8d811fd29d740daf3cd39ce
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-14 11:14:39 +0300

    refactor: refactor 'AuthorApplicationMapper' and 'BookApplicationMapper'

--- src/main/java/com/deveyk/bookstore/author/model/mapper/AuthorApplicationMapper.java
+++ src/main/java/com/deveyk/bookstore/author/model/mapper/AuthorApplicationMapper.java
@@ -7,6 +7,7 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface AuthorApplicationMapper {
 
+    // Author -> AuthorEntity
     Author toDomain(AuthorEntity entity);
 
 }
--- src/main/java/com/deveyk/bookstore/book/model/mapper/BookApplicationMapper.java
+++ src/main/java/com/deveyk/bookstore/book/model/mapper/BookApplicationMapper.java
@@ -9,12 +9,15 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface BookApplicationMapper {
 
+    // BookCreateCommand -> Book
     @Mapping(target = "genres", ignore = true)
     @Mapping(target = "authors", ignore = true)
     Book toDomain(BookCreateCommand command);
 
+    // BookEntity -> Book
     Book toDomain(BookEntity entity);
 
+    // Book -> BookEntity
     BookEntity toEntity(Book book);
 
 }
