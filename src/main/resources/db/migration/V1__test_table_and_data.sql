112ec6646bb19156d1e8f9aa57007f83b90d498f
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-04-28 22:18:45 +0300

    refactor: refactor db migration

--- src/main/resources/db/migration/V1__test_table_and_data.sql
+++ src/main/resources/db/migration/V1__test_table_and_data.sql
@@ -1,3 +1,17 @@
+CREATE TABLE IF NOT EXISTS BS_CATEGORIES (
+    id           VARCHAR(255) PRIMARY KEY,
+    name         VARCHAR(255) NOT NULL,
+    description  VARCHAR(255),
+    status       VARCHAR(255) NOT NULL CHECK ( status IN
+                                               ('ACTIVE', 'INACTIVE', 'DELETED') ),
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by   VARCHAR(255),
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_by   VARCHAR(255)
+
+);
+
+
 CREATE TABLE IF NOT EXISTS BS_BOOKS (
     id              VARCHAR(255) PRIMARY KEY,
     title           VARCHAR(255),
@@ -8,14 +22,18 @@ CREATE TABLE IF NOT EXISTS BS_BOOKS (
     edition         VARCHAR(255),
     language        VARCHAR(255),
     page_count      INT,
-    category        VARCHAR(255),
+    category_id     VARCHAR(255),
     status          VARCHAR(255) NOT NULL CHECK ( status IN
                                                   ('AVAILABLE', 'UNAVAILABLE', 'DELETED')),
-
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by      VARCHAR(255),
     updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_by      VARCHAR(255)
+    updated_by      VARCHAR(255),
+
+    -- Foreign key constraint
+    CONSTRAINT fk_bs_books_category
+                                    FOREIGN KEY (category_id)
+                                    REFERENCES BS_CATEGORIES(id)
 );
 
 CREATE TABLE IF NOT EXISTS BS_AUTHORS
@@ -101,35 +119,51 @@ CREATE TABLE IF NOT EXISTS BS_BOOK_GENRES
             ON DELETE CASCADE
 );
 
+-- BS_CATEGORIES
+INSERT INTO BS_CATEGORIES (id, name, description, status, created_at, created_by, updated_at, updated_by)
+VALUES ('350e8400-e29b-41d4-a716-446655440000', 'Fiction', 'Books about fantasy and science fiction', 'ACTIVE',
+        '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 'admin');
+INSERT INTO BS_CATEGORIES (id, name, description, status, created_at, created_by, updated_at, updated_by)
+VALUES ('350e8400-e29b-41d4-a716-446655440001', 'Non-Fiction', 'Books about non-fiction', 'INACTIVE',
+        '2023-01-01 00:00:00', 'admin', '2023-01-01 00:00:00', 'admin');
+
+
 -- BS_BOOKS
-INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count, category,
+INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count,
+                      category_id,
                       status)
 VALUES ('550e8400-e29b-41d4-a716-446655440000', 'The Hobbit', '0345339681', '9780345339683', 'George Allen & Unwin',
-        '1937-09-21', '1st', 'English', 310, 'category1', 'AVAILABLE');
-INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count, category,
+        '1937-09-21', '1st', 'English', 310, '350e8400-e29b-41d4-a716-446655440000', 'AVAILABLE');
+INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count,
+                      category_id,
                       status)
 VALUES ('550e8400-e29b-41d4-a716-446655440001', 'The Lord of the Rings', '0618640150', '9780618640157',
-        'George Allen & Unwin', '1954-07-29', '1st', 'English', 1178, 'category1', 'AVAILABLE');
-INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count, category,
+        'George Allen & Unwin', '1954-07-29', '1st', 'English', 1178, '350e8400-e29b-41d4-a716-446655440000', 'AVAILABLE');
+INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count,
+                      category_id,
                       status)
 VALUES ('550e8400-e29b-41d4-a716-446655440002', 'The Hitchhiker''s Guide to the Galaxy', '0345391802', '9780345391803',
-        'Pan Books', '1979-10-12', '1st', 'English', 224, 'category1', 'AVAILABLE');
-INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count, category,
+        'Pan Books', '1979-10-12', '1st', 'English', 224, '350e8400-e29b-41d4-a716-446655440000', 'AVAILABLE');
+INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count,
+                      category_id,
                       status)
 VALUES ('550e8400-e29b-41d4-a716-446655440003', 'The Alchemist', '0061122416', '9780061122415', 'HarperOne',
-        '1988-06-01', '1st', 'English', 208, 'category2', 'AVAILABLE');
-INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count, category,
+        '1988-06-01', '1st', 'English', 208, '350e8400-e29b-41d4-a716-446655440001', 'AVAILABLE');
+INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count,
+                      category_id,
                       status)
 VALUES ('550e8400-e29b-41d4-a716-446655440004', 'The Da Vinci Code', '0307474275', '9780307474278', 'Doubleday',
-        '2003-03-18', '1st', 'English', 480, 'category2', 'AVAILABLE');
-INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count, category,
+        '2003-03-18', '1st', 'English', 480, '350e8400-e29b-41d4-a716-446655440001', 'AVAILABLE');
+INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count,
+                      category_id,
                       status)
 VALUES ('550e8400-e29b-41d4-a716-446655440005', 'The Grapes of Wrath', '0140281622', '9780140281620',
-        'The Viking Press', '1939-04-14', '1st', 'English', 464, 'category3', 'AVAILABLE');
-INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count, category,
+        'The Viking Press', '1939-04-14', '1st', 'English', 464, '350e8400-e29b-41d4-a716-446655440001', 'AVAILABLE');
+INSERT INTO BS_BOOKS (id, title, isbn_10, isbn_13, publisher, publication_date, edition, language, page_count,
+                      category_id,
                       status)
 VALUES ('550e8400-e29b-41d4-a716-446655440006', 'The Little Prince', '0156012197', '9780156012195',
-        'Éditions Gallimard', '1943-04-06', '1st', 'English', 96, 'category4', 'AVAILABLE');
+        'Éditions Gallimard', '1943-04-06', '1st', 'English', 96, '350e8400-e29b-41d4-a716-446655440000', 'AVAILABLE');
 
 
 -- BS_AUTHORS
