03de1f63d4580dcdcce7085822bccdca80a75a21
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-05-01 22:09:03 +0300

    feat: add 'CategoryStatus'

--- src/main/java/com/deveyk/bookstore/category/model/enums/CategoryStatus.java
+++ src/main/java/com/deveyk/bookstore/category/model/enums/CategoryStatus.java
@@ -1,7 +1,27 @@
 package com.deveyk.bookstore.category.model.enums;
 
 public enum CategoryStatus {
-    ACTIVE,
-    INACTIVE,
-    DELETED
+
+    ACTIVE {
+        @Override
+        public boolean canTransitionTo(CategoryStatus targetStatus) {
+            return targetStatus == INACTIVE;
+        }
+    },
+
+    INACTIVE {
+        @Override
+        public boolean canTransitionTo(CategoryStatus targetStatus) {
+            return targetStatus == ACTIVE;
+        }
+    },
+
+    DELETED {
+        @Override
+        public boolean canTransitionTo(CategoryStatus targetStatus) {
+            return false;
+        }
+    };
+
+    public abstract boolean canTransitionTo(CategoryStatus targetStatus);
 }
