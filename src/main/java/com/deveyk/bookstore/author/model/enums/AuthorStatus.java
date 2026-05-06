699f30c9624c10be00ecb66c48b6d086e8419d58
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-05-06 22:44:22 +0300

    refactor: refactor 'AuthorStatus' and 'ContactMethod'

--- src/main/java/com/deveyk/bookstore/author/model/enums/AuthorStatus.java
+++ src/main/java/com/deveyk/bookstore/author/model/enums/AuthorStatus.java
@@ -1,9 +1,46 @@
 package com.deveyk.bookstore.author.model.enums;
 
 public enum AuthorStatus {
-    ACTIVE,
-    INACTIVE,
-    RETIRED,
-    DECEASED,
-    SUSPENDED
+
+    ACTIVE {
+        @Override
+        public boolean canTransitionTo(AuthorStatus targetStatus) {
+            return targetStatus == INACTIVE
+                    || targetStatus == RETIRED
+                    || targetStatus == SUSPENDED;
+        }
+    },
+
+    INACTIVE {
+        @Override
+        public boolean canTransitionTo(AuthorStatus targetStatus) {
+            return targetStatus == ACTIVE
+                    || targetStatus == SUSPENDED;
+        }
+    },
+
+    RETIRED {
+        @Override
+        public boolean canTransitionTo(AuthorStatus targetStatus) {
+            return targetStatus == ACTIVE;
+        }
+    },
+
+    SUSPENDED {
+        @Override
+        public boolean canTransitionTo(AuthorStatus targetStatus) {
+            return targetStatus == ACTIVE
+                    || targetStatus == INACTIVE;
+        }
+    },
+
+    DECEASED {
+        @Override
+        public boolean canTransitionTo(AuthorStatus targetStatus) {
+            return false;
+        }
+    };
+
+    // STATE MACHINE ???????
+    public abstract boolean canTransitionTo(AuthorStatus targetStatus);
 }
--- src/main/java/com/deveyk/bookstore/author/model/enums/ContactMethod.java
+++ src/main/java/com/deveyk/bookstore/author/model/enums/ContactMethod.java
@@ -1,9 +1,47 @@
 package com.deveyk.bookstore.author.model.enums;
 
+import com.deveyk.bookstore.author.service.domain.AuthorContact;
+
 public enum ContactMethod {
-    EMAIL,
-    PHONE,
-    AGENT,
-    SOCIAL_MEDIA,
-    ALL
+
+    EMAIL {
+        @Override
+        public boolean isAvailableFor(AuthorContact contact) {
+            return contact.hasEmail();
+        }
+    },
+
+    PHONE {
+        @Override
+        public boolean isAvailableFor(AuthorContact contact) {
+            return contact.hasPhone();
+        }
+    },
+
+    AGENT {
+        @Override
+        public boolean isAvailableFor(AuthorContact contact) {
+            return contact.hasAgentContact();
+        }
+    },
+
+    SOCIAL_MEDIA {
+        @Override
+        public boolean isAvailableFor(AuthorContact contact) {
+            return contact.hasSocialMediaContact();
+        }
+    },
+
+    ALL {
+        @Override
+        public boolean isAvailableFor(AuthorContact contact) {
+            return contact.hasEmail()
+                    || contact.hasPhone()
+                    || contact.hasAgentContact()
+                    || contact.hasSocialMediaContact();
+        }
+    };
+
+    // STATE MACHINE ???
+    public abstract boolean isAvailableFor(AuthorContact contact);
 }
