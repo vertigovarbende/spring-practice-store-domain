b9a4d467e5b064cf4925bd1356356fd4d3230dcd
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-05-11 23:47:10 +0300

    feat: add 'AuthorApplicationMapper'

--- src/main/java/com/deveyk/bookstore/author/model/mapper/AuthorApplicationMapper.java
+++ src/main/java/com/deveyk/bookstore/author/model/mapper/AuthorApplicationMapper.java
@@ -1,13 +1,21 @@
 package com.deveyk.bookstore.author.model.mapper;
 
 import com.deveyk.bookstore.author.repository.entity.AuthorEntity;
+import com.deveyk.bookstore.author.service.command.AuthorCreateCommand;
 import com.deveyk.bookstore.author.service.domain.Author;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AuthorApplicationMapper {
 
+    // AuthorCreateCommand -> Author
+    Author toDomain(AuthorCreateCommand command);
+
     // Author -> AuthorEntity
     Author toDomain(AuthorEntity entity);
+
+    // AuthorEntity -> Author
+    AuthorEntity toEntity(Author author);
 
 }
