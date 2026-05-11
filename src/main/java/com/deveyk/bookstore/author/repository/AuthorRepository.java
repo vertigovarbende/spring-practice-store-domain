20bccdaad45568f62b723061c1735cd5f2ebbbe6
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-05-11 23:47:37 +0300

    refactor: refactor 'AuthorRepository'

--- src/main/java/com/deveyk/bookstore/author/repository/AuthorRepository.java
+++ src/main/java/com/deveyk/bookstore/author/repository/AuthorRepository.java
@@ -1,11 +1,13 @@
 package com.deveyk.bookstore.author.repository;
 
 import com.deveyk.bookstore.author.repository.entity.AuthorEntity;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<AuthorEntity, String> {
+public interface AuthorRepository extends JpaRepository<AuthorEntity, String>, Specification<AuthorEntity> {
 
+    boolean existsByContactEmailIgnoreCase(String email);
 
 }
